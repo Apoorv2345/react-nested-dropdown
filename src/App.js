516b9b93d53028a5c204e4a5d2fb6b507ab3067e
Apoorv <srivastava.apoorv@naukri.com>
2018-07-02 18:02:47 +0530

    git commit for nested dropdowns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import NestedDD from "./NestedDD/NestedDD.js";
+import NestedDD from "./NestedDD/index.js";
 import './App.css';
 
 class App extends Component {
